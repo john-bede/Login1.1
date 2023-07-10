290076708a3763dc16e7ff372d3186fc6c609e87
john-bede <99330216+john-bede@users.noreply.github.com>
2023-07-10 16:49:25 +0300

    updated the process-signup validation for all correctly

--- process-signup.php
+++ process-signup.php
@@ -1,3 +1,24 @@
 <?php
 
+if(empty($_POST["name"])){
+    die("Name is required");
+}
+if(! filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)){
+    die("Valid email is required");
+}
+if(strlen($_POST["password"])<8){
+    die("password must be atleast 8 characters");
+}
+if(! preg_match("/[a-z]/i", $_POST["password"])){
+    die("password must contain atleast one letter");
+}
+
+if(! preg_match("/[1-9]/",$_POST["password"])){
+    die("Password must contain atleast one number");
+}
+
+if($_POST["password"] !== $_POST["password_confirmation"]){
+    die("Passwords must match");
+}
+
 print_r($_POST);
