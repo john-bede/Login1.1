0a5e2c67c55864958f67114f1cc3b2412d229819
john-bede <99330216+john-bede@users.noreply.github.com>
2023-07-11 12:27:39 +0300

    Successful fetch after you prompt a login but no controlled output

--- login.php
+++ login.php
@@ -1,5 +1,20 @@
 <?php
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $mysqli = require __DIR__ . "/database.php";
+
+    $sql=sprintf("SELECT * FROM user WHERE email = '%s'",
+    $mysqli->real_escape_string($_POST["email"]));
+
+    $result=$mysqli->query($sql);
+
+    $user = $result->fetch_assoc();
+
+    var_dump($user);
+    exit;
+}
+
 ?>
 
 <!DOCTYPE html>
