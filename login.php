367868f598e86aaf4f14d06fb242893a8d1b35be
john-bede <99330216+john-bede@users.noreply.github.com>
2023-07-11 10:03:18 +0300

    added a login page

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="water.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login.php</title>
+</head>
+<body>
+
+    <h1>Login</h1>
+
+    <form method="post">
+        <label for="email">email</label>
+        <input type="email" name="email" id="email">
+
+        <label for="password">password</label>
+        <input type="password" name="password" id="password">
+
+        <button>Log in</button>
+    </form>
+
+</body>
+</html>
